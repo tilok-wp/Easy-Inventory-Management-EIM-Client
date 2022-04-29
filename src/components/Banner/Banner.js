87f6783adf3809b78fc039e0591ab896242b8e4f
Tilok Paul <tilokpaul.web@gmail.com>
2022-04-29 14:27:10 +0600

    Create footer section

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,16 +4,16 @@ import inventoryImage from '../../images/inventory-banner-image.png';
 const Banner = () => {
     return (
         <section className='container md:grid grid-cols-2 mx-auto'>
-            <div className='flex flex-col justify-center px-5'>
+            <div className='flex justify-center'>
+                <img className='img-fluid' src={inventoryImage} alt="InventoryImage" />
+            </div>
+            <div className='flex flex-col justify-center px-5 '>
                 <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, corporis!</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, modi. Cum illum quis quasi distinctio asperiores. Quam repudiandae aliquam facere dolorem, architecto, libero perferendis quo dignissimos a corrupti, ipsam magni!</p>
                 <div>
                     <a href="#" className='bg-color-primary hover:bg-gray-800 px-10 py-3 mt-3 text-white rounded inline-block'>Details about </a>
 
                 </div>
-            </div>
-            <div className='flex justify-center'>
-                <img className='img-fluid' src={inventoryImage} alt="InventoryImage" />
             </div>
         </section>
     );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <p>This is footer part</p>
-        </div>
+        <footer className='bg-gray-200 py-5'>
+            <div className='container mx-auto md:flex md:justify-between text-center'>
+                <div>
+                    <a href="#" target="_blank" >Facebook</a>
+                    <a className='mx-5' href="#" target="_blank" >Youtube</a>
+                    <a href="#" target="_blank" >Twitter</a>
+                </div>
+                <div className='md:order-3'>
+                    <Link className='mr-3' to='/privacy'> privacy</Link>
+                    <Link to='/terms-condition'> terms & condition</Link>
+                </div>
+                <div className='md:order-2'>
+                    <p>Copyright &copy;{new Date().getFullYear()} <strong><Link to={'/'} title="Easy Inventory Management">EIM</Link></strong> all right reserved.</p>
+                </div>
+
+            </div>
+        </footer>
     );
 };
 
