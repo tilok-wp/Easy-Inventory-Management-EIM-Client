7822ef131c02857041ef5c3f3a448b78c40a4fc3
Tilok Paul <tilokpaul.web@gmail.com>
2022-04-29 09:04:11 +0600

    Install necessary libery and create file folder stracture

---
+++ src/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            this is banner part
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h3>This is header part</h3>
+        </div>
+    );
+};
+
+export default Header;
