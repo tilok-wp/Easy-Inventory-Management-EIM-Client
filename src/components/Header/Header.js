fc739e17ea2a4e5ee27a71ab540ccaaaa70acf9a
Tilok Paul <tilokpaul.web@gmail.com>
2022-05-04 23:56:46 +0600

    Update sign in and sign out option in header menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,15 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ActiveLink from '../../utility/ActiveLink/ActiveLink';
 import { XIcon, MenuAlt2Icon } from '@heroicons/react/solid'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
+    const [user] = useAuthState(auth)
+    const signOutHandle = () => {
+        signOut(auth)
+    }
     const [toogle, setToogle] = useState(false)
     return (
         <header className='py-5 bg-white sticky top-0 z-10 shadow-lg shadow-slate-100'>
@@ -17,7 +24,9 @@ const Header = () => {
                     <ActiveLink to='/blogs'>Blogs</ActiveLink>
                 </nav>
                 <div className='fixed top-3 right-0 flex items-center md:static z-50'>
-                    <Link to='/login'><button className='bg-color-primary font-semibold hover:bg-gray-800 px-5 py-1.5 ml-3 text-white rounded-lg'>Login</button></Link>
+                    {
+                        user ? <> <button onClick={signOutHandle} className='bg-color-primary font-semibold hover:bg-gray-800 px-5 py-1.5 ml-3 text-white rounded-full'>Sign out </button><img className='w-10 h-10 border-2 rounded-full' src={user?.photoURL} alt="" /></> : <Link to='/login'><button className='bg-color-primary font-semibold hover:bg-gray-800 px-5 py-1.5 ml-3 text-white rounded-full'>Login</button></Link>
+                    }
                     <div onClick={() => setToogle(!toogle)} className='w-8 h-8 md:hidden ml-5 mr-5'>
                         {toogle ? <XIcon /> : <MenuAlt2Icon />}
                     </div>
