b80957fe923a087c6b7ac50553731a3f61610a64
Tilok Paul <tilokpaul.web@gmail.com>
2022-05-05 22:14:35 +0600

    Update readme content and finally solve the issue to hosting firebase

---
+++ src/components/Blog/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const blog = () => {
+    return (
+        <div>
+            <h3>Blog heading </h3>
+            <p>Blog description Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit praesentium quidem optio recusandae eveniet, iusto nam sunt mollitia possimus porro.</p>
+        </div>
+    );
+};
+
+export default blog;
--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -3,28 +3,31 @@ import { FolderAddIcon, ViewBoardsIcon, PuzzleIcon, DocumentAddIcon, TrashIcon, 
 
 const Features = () => {
     return (
-        <section className='container mx-auto my-10 py-20 bg-sky-50 px-3 md:px-0'>
-            <h3 className='text-center text-3xl uppercase mb-10'>Features</h3>
-            <div className='grid gap-5 md:grid-cols-2 lg:grid-cols-5'>
-                <div className='border border-slate-100 bg-white rounded-2xl py-10 flex flex-col justify-center items-center'>
-                    <AdjustmentsIcon className="h-14 w-14 text-color-primary hover:text-gray-800" />
-                    <h3 className='lg:text-xl'>Manage all product</h3>
-                </div>
-                <div className='border border-slate-100 bg-white rounded-2xl py-10 flex flex-col justify-center items-center'>
-                    <DocumentAddIcon className="h-14 w-14 text-color-primary hover:text-gray-800" />
-                    <h3 className='lg:text-xl'>Product Add</h3>
-                </div>
-                <div className='border border-slate-100 bg-white rounded-2xl py-10 flex flex-col justify-center items-center'>
-                    <TrashIcon className="h-14 w-14 text-color-primary hover:text-gray-800" />
-                    <h3 className='lg:text-xl'>Product Delete</h3>
-                </div>
-                <div className='border border-slate-100 bg-white rounded-2xl py-10 flex flex-col justify-center items-center'>
-                    <RefreshIcon className="h-14 w-14 text-color-primary hover:text-gray-800" />
-                    <h3 className='lg:text-xl'>Product update</h3>
-                </div>
-                <div className='border border-slate-100 bg-white rounded-2xl py-10 flex flex-col justify-center items-center'>
-                    <TruckIcon className="h-14 w-14 text-color-primary hover:text-gray-800" />
-                    <h3 className='lg:text-xl'>Product delivered</h3>
+        <section className='my-10 py-20 bg-sky-50 px-3 md:px-0'>
+            <div className='container mx-auto'>
+                <h3 className='text-center text-3xl uppercase mb-10'>Features</h3>
+                <div className='grid gap-5 md:grid-cols-2 lg:grid-cols-5'>
+                    <div className='border border-slate-100 bg-white rounded-2xl py-10 flex flex-col justify-center items-center'>
+                        <AdjustmentsIcon className="h-14 w-14 text-color-primary hover:text-gray-800" />
+                        <h3 className='lg:text-xl'>Manage all product</h3>
+                    </div>
+                    <div className='border border-slate-100 bg-white rounded-2xl py-10 flex flex-col justify-center items-center'>
+                        <DocumentAddIcon className="h-14 w-14 text-color-primary hover:text-gray-800" />
+                        <h3 className='lg:text-xl'>Product Add</h3>
+                    </div>
+                    <div className='border border-slate-100 bg-white rounded-2xl py-10 flex flex-col justify-center items-center'>
+                        <TrashIcon className="h-14 w-14 text-color-primary hover:text-gray-800" />
+                        <h3 className='lg:text-xl'>Product Delete</h3>
+                    </div>
+                    <div className='border border-slate-100 bg-white rounded-2xl py-10 flex flex-col justify-center items-center'>
+                        <RefreshIcon className="h-14 w-14 text-color-primary hover:text-gray-800" />
+                        <h3 className='lg:text-xl'>Product update</h3>
+                    </div>
+                    <div className='border border-slate-100 bg-white rounded-2xl py-10 flex flex-col justify-center items-center'>
+                        <TruckIcon className="h-14 w-14 text-color-primary hover:text-gray-800" />
+                        <h3 className='lg:text-xl'>Product delivered</h3>
+                    </div>
+
                 </div>
 
             </div>
