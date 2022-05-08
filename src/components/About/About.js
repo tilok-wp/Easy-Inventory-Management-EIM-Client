d6cf58b207ac4edb311cff904c9606a4f0ba20c2
Tilok Paul <tilokpaul.web@gmail.com>
2022-05-08 20:53:32 +0600

    Update page content and design

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,11 @@ const About = () => {
     return (
         <div className='py-20'>
             <h3 className='text-center text-3xl uppercase mb-10'>About</h3>
-            <p>About company text</p>
+            <div className='md:w-1/2 mx-auto'>
+                <p>The Easy Inventory (E I M ) is a Bangladesh base dates impoter company, founded by Sir Tilok paul and evolved from a single The Warehouse store to become the country’s largest general merchandise retailer. Our brands include The Warehouse, Warehouse Stationery, Noel Leeming, Torpedo7, and TheMarket.
+
+                    Our purpose is to help Kiwis live better every day. Our vision is to build Dhaka Banglasesh most sustainable, convenient and customer-first company.</p>
+            </div>
 
             <PageTitleBar title='About'></PageTitleBar>
         </div>
--- src/components/TermsCondition/TermsCondition.js
+++ src/components/TermsCondition/TermsCondition.js
@@ -3,8 +3,18 @@ import PageTitleBar from '../PageTitleBar/PageTitleBar';
 
 const TermsCondition = () => {
     return (
-        <div>
-            <h3 className='text-center text-3xl uppercase mb-10'>Tearms & condition page</h3>
+        <div className='py-20 md:w-3/4 mx-auto'>
+            <h3 className='text-center text-3xl uppercase mb-10'>Tarms and condition</h3>
+            <h4 className='text-xl mt-5'>Some terms and condition as follows:</h4>
+
+            <p>
+
+                f you have a website or software application, you will likely need to have Terms of Service or as some people call it – Terms of Use for it. Terms of Service or Terms and Conditions (T&C) will, in legal terms, limit your business or personal liability.
+
+                It is highly recommended to have robust and comprehensive Terms and Conditions for any website, online business, or application. It will provide you with proper protection in case some of your customers or users will decide to take legal action against your business.
+
+                The best practice is only to allow using your service to people that agree with your Terms & Conditions, Disclaimer and Privacy Policy. This way, your website, business, or app will be protected from potential legal dangers.
+            </p>
             <PageTitleBar title='Terms&Condition'></PageTitleBar>
         </div>
     );
